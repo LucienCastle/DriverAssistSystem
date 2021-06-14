5999340cf78ac4030f6b91cddecc820d59a3784f
LucienCastle <47452095+LucienCastle@users.noreply.github.com>
2021-06-14 22:21:17 +0530

    added distance calculation

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""@author: kyleguan
-"""
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-# from moviepy.editor import VideoFileClip
-from collections import deque
-# from sklearn.utils.linear_assignment_ import linear_assignment
-from scipy.optimize import linear_sum_assignment as linear_assignment
-import time
-import helpers 
-from helpers import box_iou2
-import detector
-from tracker import Tracker
+
 import lane
 from lane import vid_pipeline
 
+import time
 
 import pandas as pd
 from packages import object_config as config
@@ -41,77 +30,126 @@ from traffic import YoloV3
 from traffic import transform_images
 from traffic import draw_outputs
 
+
+# load classes for object detection
 labelsPath =os.path.sep.join([config.MODEL_PATH,"coco.names"])
 LABELS =open(labelsPath).read().strip().split("\n")
 
+# load weights for object detection
 weightsPath =os.path.sep.join([config.MODEL_PATH,"yolov2-tiny.weights"])
 
+# load config file for object detection
 configPath= os.path.sep.join([config.MODEL_PATH,"tiny.cfg"])
 print("[INFO] loading YOLO from disk")
 
+# init dnn using loaded weights
 net =cv2.dnn.readNetFromDarknet(configPath,weightsPath)
 
+
 if config.USE_GPU:
+	# checks if GPU is available for usage
 	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
+# get layers
 ln = net.getLayerNames()
 
 ln = [ln[i[0]-1]for i in net.getUnconnectedOutLayers()]
 
+# filter relevant objects for detection
 objects_list = []
 for i in [2,5,7,9,11]:
     objects_list.append(LABELS[i])
 
 def euclidean(x,y,dst_size=(700,400),src=np.float32([(0,0.5),(1,0.5),(0,1),(1,1)]),dst=np.float32([(0,0),(1,0),(0,1),(1,1)])):
+	'''
+	calculates the euclidean distance between ego vehicle and the object using perspective transform
+
+	:params:
+	 x,y      : coordinates of vehicle centroid
+	 dst_size : destination values for perspective transform
+	 src      : source points for transform
+	 dst      : destination points for transform
+
+	:returns:
+	 distance between ego vehicle and detected object/vehicle
+	'''
+
+	# claculate source points
 	img_size = np.float32([(700,400)])
 	src = src*img_size
 
+	# calculate destination points
 	dst = dst*np.float32(dst_size)
+
+	# get transformation matrix
 	matrix = cv2.getPerspectiveTransform(src,dst)
 
+	# transform points
 	p = np.array([x,y], np.float32)
 	p = p.reshape(-1,1,2).astype(np.float32)
 	transformed_points_obj = cv2.perspectiveTransform(p, matrix)
 	pts = transformed_points_obj[0][0]
-	ym_per_pix = 300/350
-	xm_per_pix = 1.8/480
-	return math.sqrt(pow(pts[0]*xm_per_pix-350*xm_per_pix,2)+pow(2,pts[1]*ym_per_pix-400*ym_per_pix))
+	
+	# define meters per pixel in X and Y direction
+	ym_per_pix = 30.7/480
+	xm_per_pix = 3.6/640
+
+	return math.sqrt(pow(pts[0]*xm_per_pix-700*xm_per_pix,2)+pow(pts[1]*ym_per_pix-400*ym_per_pix,2))
 
 def predict_objects(img):
-    frame = imutils.resize(img,width =700, height=400)
-    print(frame.shape)
-    labels=[]
-    labels.append(objects_list)
+	'''
+	predicts the bounding box, class and location of object
 
-    for label in objects_list:
-        results = detect_people(frame,net,ln,personIdx=LABELS.index(label))
-                   
-        for(i,(prob,bbox,centroid)) in enumerate(results):
-            (startX,startY,endX,endY)=bbox
-            (cX,cY)=centroid 
-            print(cX,cY)
-            if ( cY > 250) or ( cX < 360 and cX > 350):
-            	color = (0,0,255)
-            else:
-            	color=(0,255,0)
+	:params:
+	 img : image frame
 
-            dist = euclidean(cX,cY)
-            	
-            cv2.rectangle(frame,(startX,startY),(endX,endY),color,2)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontSize = 0.5
-            fontColor = (0,0,0)
-            cv2.putText(frame,'Dist:{:.4f} m '.format(dist*0.01),(startX,startY),font,fontSize,fontColor,2)
-    return frame
+	:returns:
+	 frame : image with detected object
+	'''
+	frame = imutils.resize(img,width =700, height=400)
+	labels=[]
+	labels.append(objects_list)
+
+	for label in objects_list:
+		results = detect_people(frame,net,ln,personIdx=LABELS.index(label))
+
+		for(i,(prob,bbox,centroid)) in enumerate(results):
+			(startX,startY,endX,endY)=bbox
+			(cX,cY)=centroid
+			print(cX,cY)
+			
+			if ( cY > 250) or ( cX < 360 and cX > 350):
+				color = (0,0,255)
+			else:
+				color=(0,255,0)
+			# calculate distance between ego vehicle and object
+			dist = euclidean(cX,cY)
+			# draw the predictions
+			cv2.rectangle(frame,(startX,startY),(endX,endY),color,2)
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			fontSize = 0.5
+			fontColor = (0,0,0)
+			cv2.putText(frame,'Dist:{:.4f} m '.format(dist),(startX,startY),font,fontSize,fontColor,2)
+	return frame
 
 def Convertltd(lst): 
 	res_dct = {i: lst[i] for i in range(0, len(lst))} 
 	return res_dct
 
-def traffic_pipeline(i):
+def traffic_pipeline(img_raw):
+	'''
+	pipeline function for traffic sign detection and recognition by performing transformations
+
+	:params:
+	 i : index of frame
+
+	:returns:
+	 predictions
+	'''
+
 	size = 416
-	img_raw = tf.image.decode_image(open(interPath+str(i)+'.jpg', 'rb').read(), channels=3)
+	# img_raw = tf.image.decode_image(open(interPath+str(i)+'.jpg', 'rb').read(), channels=3)
 	img = tf.expand_dims(img_raw, 0)
 	img = transform_images(img, size)
 	boxes, scores, classes1, nums = modell(img)
@@ -120,24 +158,25 @@ def traffic_pipeline(i):
 
    
 if __name__ == "__main__":
+	# load classes for traffic sign detection and recognition
 	classes1 = 'obj.names'
 	class_names = [c.strip() for c in open(classes1).readlines()]
 
+	# load weights for traffic sign detection and recognition
 	with open ('lr001_2_1_5', 'rb') as fp:
 		weights = pickle.load(fp)
 
+	# initialize model with loaded weights
 	modell = YoloV3(training=False)
 	modell.set_weights(weights)
 
 	start=time.time()
 	output = 'test_v9.avi'
 	
-
-	cap=cv2.VideoCapture("project_video.mp4")
+	# open video file to be tested
+	cap=cv2.VideoCapture("challenge_video.mp4")
 	interPath = 'inter/'
 	
-
-
 	if cap.isOpened():
 		w=int(cap.get(3))
 		h=int(cap.get(4))
@@ -150,11 +189,17 @@ if __name__ == "__main__":
 			ret,frame=cap.read()
 			if ret == True:
 				i = i + 1
+				cv2.imwrite(interPath+'interImg.jpg', frame)
+
+				# call pipeline for lane detection
 				img = vid_pipeline(frame)
+
+				# detect objects 
 				img = predict_objects(img)
 				
-				# outputs = traffic_pipeline(i)
-				# img = draw_outputs(img, outputs, class_names)
+				img_raw = tf.image.decode_image(open(interPath+str(i)+'.jpg', 'rb').read(), channels=3)
+				outputs = traffic_pipeline(img_raw)
+				img = draw_outputs(img, outputs, class_names)
 
 				img = cv2.resize(img, (640, 480))
 				video.write(img)
