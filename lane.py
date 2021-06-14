c7f905437d408c8c17c3636a084e0ef4c45f165b
LucienCastle <47452095+LucienCastle@users.noreply.github.com>
2021-06-14 22:46:17 +0530

    lane departure warning

--- lane.py
+++ lane.py
@@ -11,6 +11,17 @@ left_a,left_b,left_c=[],[],[]
 right_a,right_b,right_c=[],[],[]
 
 def pipeline(img,s_thresh=(100,255),sx_thresh=(15,255)):
+    '''
+    pipeline for image preprocessing
+
+    :params:
+     img       : image frame
+     s_thresh  : threshold for schannel filter
+     sx_thresh : threshold for sobel operator
+
+    :returns:
+     combined_binary : image with detected edges
+    '''
     img=undistort_img(img,'calibration_pickle.p',False)
     img=np.copy(img)
 
@@ -36,6 +47,18 @@ def pipeline(img,s_thresh=(100,255),sx_thresh=(15,255)):
     return combined_binary
 
 def perspective_warp(img,dst_size=(1280,720),src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),dst=np.float32([(0,0),(1,0),(0,1),(1,1)])):
+    '''
+    perspective transform to get birds-eye-view
+
+    :params:
+     img      : image
+     dst_size : size of transformed image
+     src      : source points
+     dst      : destination points
+
+    :returns:
+     warped : warped image
+    '''
     img_size=np.float32([(img.shape[1],img.shape[0])])
     src=src*img_size
     dst=dst*np.float32(dst_size)
@@ -45,6 +68,18 @@ def perspective_warp(img,dst_size=(1280,720),src=np.float32([(0.43,0.65),(0.58,0
     return warped
 
 def inv_perspective_warp(img,dst_size=(1280,720),src=np.float32([(0,0),(1,0),(0,1),(1,1)]),dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
+    '''
+    inverse perspective transform to get back the points in original image space
+
+    :params:
+     img      : image frame
+     dst_size : size of destination image transform
+     src      : source points
+     dst      : destination points
+
+    :returns:
+     warped : inverse transformed image
+    '''
     img_size=np.float32([(img.shape[1],img.shape[0])])
     src=src*img_size
     dst=dst*np.float32(dst_size)
@@ -54,10 +89,36 @@ def inv_perspective_warp(img,dst_size=(1280,720),src=np.float32([(0,0),(1,0),(0,
     return warped
 
 def get_hist(img):
+    '''
+    construct histogram
+
+    :params:
+     img : image frame
+
+    :returns:
+     hist : histogram of the image
+    '''
     hist=np.sum(img[img.shape[0]//2:,:],axis=0)
     return hist
 
 def sliding_window(img,nwindows=9,margin=150,minpix=1,draw_windows=True):
+    '''
+    we use sliding window to track the lane pixels in a given frame
+
+    :params:
+     img          : image frame
+     nwindows     : number of sliding windows
+     margin       : margin for window
+     minpix       : minimum pixel
+     draw_windows : shows windows on image
+
+    :returns:
+     out_img    : output image with detected lanes
+     left_fitx  : left lane
+     right_fitx : right lane
+     left_fit   : constants of left polynomial
+     right_fit  : constants of right polynomial
+    '''
     global left_a,left_b,left_c,right_a,right_b,right_c
     left_fit_=np.empty(3)
     right_fit_=np.empty(3)
@@ -106,49 +167,6 @@ def sliding_window(img,nwindows=9,margin=150,minpix=1,draw_windows=True):
     left_lane_ind=np.concatenate(left_lane_ind)
     right_lane_ind=np.concatenate(right_lane_ind)
 
-    # ploty=np.linspace(0,img.shape[0]-1,img.shape[0])
-    #
-    # if len(left_lane_ind)>0:
-    #     leftx=nonzerox[left_lane_ind]
-    #     lefty=nonzeroy[left_lane_ind]
-    #
-    #     left_fit=np.polyfit(lefty,leftx,2)
-    #
-    #     left_a.append(left_fit[0])
-    #     left_b.append(left_fit[1])
-    #     left_c.append(left_fit[2])
-    #
-    #     left_fit_[0]=np.mean(left_a[-10:])
-    #     left_fit_[1]=np.mean(left_b[-10:])
-    #     left_fit_[2]=np.mean(left_c[-10:])
-    #
-    #     left_fitx=left_fit_[0]*ploty**2+left_fit_[1]*ploty+left_fit_[2]
-    #     out_img[nonzeroy[left_lane_ind],nonzerox[left_lane_ind]]=[255,0,100]
-    # else:
-    #     left_fitx=[]
-    #     left_fit_=[]
-    #
-    # if len(right_lane_ind)>0:
-    #     rightx=nonzerox[right_lane_ind]
-    #     righty=nonzeroy[right_lane_ind]
-    #
-    #     right_fit=np.polyfit(righty,rightx,2)
-    #
-    #     right_a.append(right_fit[0])
-    #     right_b.append(right_fit[1])
-    #     right_c.append(right_fit[2])
-    #
-    #     right_fit_[0]=np.mean(right_a[-10:])
-    #     right_fit_[1]=np.mean(right_b[-10:])
-    #     right_fit_[2]=np.mean(right_c[-10:])
-    #
-    #     right_fitx=right_fit_[0]*ploty**2+right_fit_[1]*ploty+right_fit_[2]
-    #     out_img[nonzeroy[right_lane_ind],nonzerox[right_lane_ind]]=[0,100,255]
-    # else:
-    #     right_fitx=[]
-    #     right_fit_=[]
-
-
     leftx=nonzerox[left_lane_ind]
     lefty=nonzeroy[left_lane_ind]
     rightx=nonzerox[right_lane_ind]
@@ -183,34 +201,23 @@ def sliding_window(img,nwindows=9,margin=150,minpix=1,draw_windows=True):
     return out_img,(left_fitx,right_fitx),(left_fit_,right_fit_),ploty
 
 def get_curve(img,leftx,rightx):
+    '''
+    calculate radius of curvature of left and right lane
+
+    :params:
+     img    : image frame
+     leftx  : left lane
+     rightx : right lane
+
+    :returns:
+     left_curve_rad  : curvature of left lane
+     right_curve_rad : curvature of right lane
+     center          : center lane
+    '''
     ploty=np.linspace(0,img.shape[0]-1,img.shape[0])
     y_eval=np.max(ploty)
     ym_per_pix=30.5/720
     xm_per_pix=3.7/720
-
-    # car_pos=img.shape[1]/2
-    #
-    # if len(leftx)>0:
-    #     left_fit_cr=np.polyfit(ploty*ym_per_pix,leftx*xm_per_pix,2)
-    #     left_curve_rad=((1+(2*left_fit_cr[0]*y_eval*ym_per_pix+left_fit_cr[1])**2)**1.5)/np.absolute(2*left_fit_cr[0])
-    #     l_fit_x_int=left_fit_cr[0]*img.shape[0]**2+left_fit_cr[1]*img.shape[0]+left_fit_cr[2]
-    # else:
-    #     left_curve_rad=0
-    #     l_fit_x_int=0
-    #
-    # if len(rightx)>0:
-    #     right_fit_cr=np.polyfit(ploty*ym_per_pix,rightx*xm_per_pix,2)
-    #     right_curve_rad=((1+(2*right_fit_cr[0]*y_eval*ym_per_pix+right_fit_cr[1])**2)**1.5)/np.absolute(2*right_fit_cr[0])
-    #     r_fit_x_int=right_fit_cr[0]*img.shape[0]**2+right_fit_cr[1]*img.shape[0]+right_fit_cr[2]
-    # else:
-    #     right_curve_rad=0
-    #     r_fit_x_int=0
-    #
-    # if leftx!=None and rightx!=None:
-    #     lane_center_pos=(r_fit_x_int+l_fit_x_int)/2
-    #     center=(car_pos-lane_center_pos)*xm_per_pix/10
-    # else:
-    #     center=0
 
     left_fit_cr=np.polyfit(ploty*ym_per_pix,leftx*xm_per_pix,2)
     right_fit_cr=np.polyfit(ploty*ym_per_pix,rightx*xm_per_pix,2)
@@ -228,24 +235,17 @@ def get_curve(img,leftx,rightx):
     return (left_curve_rad,right_curve_rad,center)
 
 def draw_lanes(img,left_fit,right_fit,Offset):
+    '''
+    draws lane
+
+    :params:
+     img       : image frame
+     left_fit  : left lane
+     right_fit : right lane
+     Offset    : offset from center lane
+    '''
     ploty=np.linspace(0,img.shape[0]-1,img.shape[0])
     color_img=np.zeros_like(img)
-
-    # if len(left_fit)>0:
-    #     left=np.array([np.transpose(np.vstack([left_fit,ploty]))])
-    # else:
-    #     left=np.zeros_like(np.vstack([right_fit,ploty]))
-    # if len(right_fit)>0:
-    #     right=np.array([np.flipud(np.transpose(np.vstack([right_fit,ploty])))])
-    # else:
-    #     right=np.zeros_like(np.vstack([left_fit,ploty]))
-    #
-    # points=np,hstack((left,right))
-    # # if left_fit!=None and right_fit!=None:
-    # if np.absolute(Offset)>0.1:
-    #     cv2.fillPoly(color_img,np.int_(points),(0,0,255))
-    # else:
-    #     cv2.fillPoly(color_img,np.int_(points),(0,255,0))
 
     left=np.array([np.transpose(np.vstack([left_fit,ploty]))])
     right=np.array([np.flipud(np.transpose(np.vstack([right_fit,ploty])))])
@@ -261,6 +261,15 @@ def draw_lanes(img,left_fit,right_fit,Offset):
     return inv_perspective
 
 def vid_pipeline(img):
+    '''
+    video pipeline for lane departure
+
+    :params:
+     img : image frame
+
+    :returns:
+     img : output image with detected lane and departure warning
+    '''
     global running_avg
     global index
     img_=pipeline(img)
@@ -278,44 +287,3 @@ def vid_pipeline(img):
     cv2.putText(img,'Vehicle Offset:{:.4f} m'.format(curve_rad[2]),(570,650),font,fontSize,fontColor,2)
 
     return img
-
-# if __name__=="__main__":
-    # img = cv2.imread('testing/test2.jpg')
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # dst = pipeline(img)
-    # plt.imshow(dst)
-    # dst = perspective_warp(dst, dst_size=(1280,720))
-    # print('dds')
-    # Visualize undistortion
-    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-    # ax1.imshow(img)
-    # ax1.set_title('Original Image', fontsize=30)
-    # ax2.imshow(dst, cmap='gray')
-    # ax2.set_title('Warped Image', fontsize=30)
-    # plt.show()
-    #
-    # out_img,curves,lanes,ploty=sliding_window(dst)
-    # print(np.asarray(curves).shape)
-    # curverad=get_curve(img, curves[0],curves[1])
-    # print(curverad)
-    # img_ = draw_lanes(img, curves[0], curves[1])
-    # plt.imshow(img_, cmap='hsv')
-    # plt.show()
-    # n=10
-    # cap=cv2.VideoCapture("challenge_video.mp4")
-    # while(cap.isOpened()):
-    #     ret,frame=cap.read()
-    #     if ret==True:
-    #         n-=1
-    #         if n==0:
-    #             n=5
-    #             img=vid_pipeline(frame)
-    #             cv2.imshow('Final',img)
-    #         # else:
-    #         #     cv2.imshow('Final',frame)
-    #         if cv2.waitKey(1)==ord('q'):
-    #             break
-    #     else:
-    #         break
-    # cap.release()
-    # cv2.destroyAllWindows()
